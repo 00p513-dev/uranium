bf72faae24c6c565b6b6d8222d078efe36739c10
Amy Grace <alexking5@protonmail.com>
2020-06-13 10:43:21 +0100

    start driver abstraction shit. note: keyboard broken in this update!

--- kernel.cpp
+++ kernel.cpp
@@ -2,7 +2,33 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "driver.h"
 #include "keyboard.h"
+
+
+void printfHex(uint8_t key)
+{
+    char* foo = "00";
+    char* hex = "0123456789ABCDEF";
+    foo[0] = hex[(key >> 4) & 0xF];
+    foo[1] = hex[key & 0xF];
+    printf(foo);
+}
+
+
+
+
+
+class PrintfKeyboardEventHandler : public KeyboardEventHandler
+{
+public:
+    void OnKeyDown(char c)
+    {
+        char* foo = " ";
+        foo[0] = c;
+        printf(foo);
+    }
+};
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
@@ -30,11 +56,26 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     char* dpid = &cpid;
     printf(dpid);
 
-    //please dont write shit beneath here, i wasted an hour of my life because of this shit
+    printf("\n");
+
+    //please dont write shit beneath here unless you know why! i wasted an hour of my life because of this shit
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
-    KeyboardDriver keyboard(&interrupts);
-    interrupts.Activate();
+
+    printf("GDT and InterruptManager loaded! Let's get some drivers! \n");
+
+    printf("Initializing Hardware, Stage 1\n");
+
+    DriverManager drvManager;
+
+        PrintfKeyboardEventHandler kbhandler;
+        KeyboardDriver keyboard(&interrupts, &kbhandler);
+        drvManager.AddDriver(&keyboard);
+
+    printf("Initializing Hardware, Stage 2\n");
+        drvManager.ActivateAll();
+
+    printf("Initializing Hardware, Stage 3\n");
 
     while(1);
 }
--- makefile
+++ makefile
@@ -7,7 +7,7 @@ ASPARAMS = --32
 LDPARAMS = -melf_i386
 
 
-objects = loader.o gdt.o port.o interruptstubs.o interrupts.o keyboard.o kernel.o
+objects = loader.o gdt.o driver.o port.o interruptstubs.o interrupts.o keyboard.o kernel.o
 
 
 
